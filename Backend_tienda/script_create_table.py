4ddfb57237603278386c4e5b9857bee2cd02f5f4
Hardila-data <67773921+Hardila-data@users.noreply.github.com>
2024-03-19 14:20:49 -0500

    Update script_create_table.py

--- Backend_tienda/script_create_table.py
+++ Backend_tienda/script_create_table.py
@@ -3,16 +3,16 @@ import sqlite3
 with sqlite3.connect('db.sqlite3') as connection:
     c = connection.cursor()
 
-    #Create the table
-    query_create_table = """
-        CREATE TABLE IF NOT EXISTS tienda(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT 
-     )
-    """
+    # #Create the table
+    # query_create_table = """
+    #     CREATE TABLE IF NOT EXISTS tienda(
+    #     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #     name TEXT 
+    #  )
+    # """
 
-    c.execute(query_create_table)
-    connection.commit()
+    # c.execute(query_create_table)
+    # connection.commit()
 
     #Create the table
     query_create_table = """
@@ -30,32 +30,37 @@ with sqlite3.connect('db.sqlite3') as connection:
     c.execute(query_create_table)
     connection.commit()
 
+    # #Create the table
+    # query_create_table = """
+    #     CREATE TABLE IF NOT EXISTS usuario(
+    #     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #     name TEXT 
+    #  )
+    # """
+
+    # c.execute(query_create_table)
+    # connection.commit()
+
+
     #Create the table
-    query_create_table = """
-        CREATE TABLE IF NOT EXISTS usuario(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT 
-     )
-    """
+    # query_create_table = """
+    #     CREATE TABLE IF NOT EXISTS compras(
+    #     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #     usuario VARCHAR(100),
+    #     id_producto INTEGER,
+    #     producto VARCHAR(200),
+    #     cantidad INTEGER,
+    #     fecha  DATETIME
+    #     )
+    # """
 
-    c.execute(query_create_table)
-    connection.commit()
+    # c.execute(query_create_table)
+    # connection.commit()
 
 
-    #Create the table
-    query_create_table = """
-        CREATE TABLE IF NOT EXISTS compras(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        usuario VARCHAR(100),
-        id_producto INTEGER,
-        producto VARCHAR(200),
-        cantidad INTEGER,
-        fecha  DATETIME
-        )
-    """
 
-    c.execute(query_create_table)
-    connection.commit()
+    # c.close()
+
 
 
 
