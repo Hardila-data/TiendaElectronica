6e6655cdd2d7bac2c604ed10967e9e98ec133afd
Hardila-data <67773921+Hardila-data@users.noreply.github.com>
2024-03-19 13:55:18 -0500

    Update urls.py

--- Backend_tienda/rest_api/urls.py
+++ Backend_tienda/rest_api/urls.py
@@ -5,5 +5,6 @@ from rest_api import views
 urlpatterns = [
     path('default', views.hello_world, name='hello_world'),
     path('productos', views.get_products, name='get_productos'),
-    path('productos/<int:id_producto>', views.get_products_by_id, name='get_products_by_id')
+    path('productos/<int:id_producto>', views.get_products_by_id, name='get_products_by_id'),
+    path('calculate_price_item', views.calculate_price_item, name='calculate_price_item')
 ]
